7201813e5786353e5ad28ba521cd45d8c690c3e5
Patricio <patriciocoronadocollado@gmail.com>
2020-05-22 17:19:04 +0200

    con Aplication para variables globales

--- app/src/main/java/com/example/mislugares5/Aplicacion.java
+++ app/src/main/java/com/example/mislugares5/Aplicacion.java
@@ -1,17 +1,18 @@
 package com.example.mislugares5;
 import android.app.Application;
 import android.content.SharedPreferences;
-
 import com.example.mislugares5.LugaresLista;
 import com.example.mislugares5.RepositorioLugares;
-
 public class Aplicacion extends Application {
-
+    //Los atributos de la clase son los objetos a los que queremos acceder
+    // En este caso un repositorio de Lugares
     public RepositorioLugares lugares = new LugaresLista();
     @Override public void onCreate() {
         super.onCreate();
     }
-
+    //Getter.....................................................
+    //Para leer el repositorio de lugares
+    //RepositorioLugares misLugares = ((Aplicacion) getApplication()).getLugares();
     public RepositorioLugares getLugares() {
         return lugares;
     }
